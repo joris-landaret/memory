aabe782c2e2414a17e0f756443084637deba41b9
joris-landaret <joris.landaret@laplateforme.io>
2023-01-04 11:07:02 +0100

    index.php : descriptif du site et du jeu

--- index.php
+++ index.php
@@ -11,7 +11,12 @@
 <body>
     <?php session_start(); include('header.php')?>
     <main>
-        
+        <p>Bienvenu sur ce site web, ici vous aurez l'opportunité de jouer au célèbre jeu memory.</p>
+        <p>Pour pouvoir jouer à MEMORY vous devrez vous inscrire et/ou vous connecter.</p>
+        <h2>Descriptif du jeu :</h2>
+        <p>Vous trouverez un certain nombre de paires de cartes faces cachées et le but est de toutes les trouver.</p>
+        <p>Pour ce faire vous devez choisir deux cartes à révéler, et si les cartes ne correspondent pas elles se retourneront faces cachées, mais si elles correspondent alors elles resteront faces visibles.</p>
+        <p>La partie se finit jusqu'à ce que toutes les cartes soient révélées.</p>
     </main>
     <?php include('footer.php')?>
 </body>
