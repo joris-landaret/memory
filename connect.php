5f2fcfda77b32564826b98a32fd1024ec49fe346
joris-landaret <joris.landaret@laplateforme.io>
2023-01-04 11:10:48 +0100

    connect.php : connexion à la base de donné

--- connect.php
+++ connect.php
@@ -1,12 +1,12 @@
 <?php
 // connexion à la base de donné
 
-$mysqli = new mysqli("localhost","root","","livreor",3307);
+$mysqli = new mysqli("localhost","root","","memory",3307);
 
-/*if ($mysqli) {
+if ($mysqli) {
     echo "connexion établie <br />";
   }
   else { 
     die(mysqli_connect_error());
-  }*/
+  }
 ?>
