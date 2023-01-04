31afb36fc0e9bebc6cb94ec91cef4c2016a305bf
joris-landaret <joris.landaret@laplateforme.io>
2023-01-04 12:00:33 +0100

    création du fichier card.php, avec la classe card

---
+++ card.php
@@ -0,0 +1,28 @@
+<?php
+
+class Card 
+{
+    // ATTRIBUT
+    public $nom;
+    public $image;
+
+    // CONSTRUCTION
+    public function __construct()
+    {
+        
+    }
+
+    // METHODE
+    public function nom($name)
+    {
+        
+    }
+
+    public function image($img)
+    {
+        
+    }
+}
+
+
+?>
