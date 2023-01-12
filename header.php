721467b175185301d51b49d528504346605f5b9d
joris-landaret <joris.landaret@laplateforme.io>
2023-01-12 10:21:36 +0100

    ajout au header le lien vers la page board

--- header.php
+++ header.php
@@ -1,22 +1,22 @@
 <header>
-        <div>
-            <img src="img/rainbow_mountain.jpg" alt="">
-            <h1>Memory</h1>
-        </div>
+    <div>
+        <img src="img/rainbow_mountain.jpg" alt="">
+        <h1>Memory</h1>
+    </div>
 
-        <div class="liste">
-            <ul>
-                <li><a href="http://memory/">Accueil</a></li>
-                <?php if (!empty($_SESSION['login'])): ?>
+    <div class="liste">
+        <ul>
+            <li><a href="http://memory/">Accueil</a></li>
+            <?php if (!empty($_SESSION['login'])) : ?>
                 <li><a href="http://memory/profil.php">Profil</a></li>
                 <li><a href="http://memory/deconnexion.php">Se déconnecter</a></li>
-                <li><a href="http://memory/commentaire">Memory</a></li>
-                <?php else: ?>
+                <li><a href="http://memory/board.php">Memory</a></li>
+            <?php else : ?>
                 <li><a href="http://memory/connexion.php">Connexion</a></li>
                 <li><a href="http://memory/inscription.php">Inscription</a></li>
-                <?php endif; ?>
-                <li><a href="http://memory/score">Tableau des scores</a></li>
-            </ul>
-        </div>
+            <?php endif; ?>
+            <li><a href="http://memory/score">Tableau des scores</a></li>
+        </ul>
+    </div>
 
-    </header>
+</header>
