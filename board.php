16b32292808854c13efb3afb316c6c5f234b2c5c
joris-landaret <joris.landaret@laplateforme.io>
2023-01-12 10:15:23 +0100

    board.php : ajouts des css de header et footer

--- board.php
+++ board.php
@@ -12,6 +12,8 @@ include "Card.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Board</title>
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="css/board.css">
 </head>
 
