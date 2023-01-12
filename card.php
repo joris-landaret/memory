4a90117657f243ad6efe208b748b4b6ffb4e53b6
joris-landaret <joris.landaret@laplateforme.io>
2023-01-12 09:29:35 +0100

    Card.php : ajouts des variables d'images pour chaque cartes

--- card.php
+++ card.php
@@ -1,5 +1,19 @@
 <?php
 
+$dos = '<form action="" method="$_POST"><input type="image" name="flip" alt="ok" class="img" src="img/back1.jpg"></form>';
+$ange = '<div class="img"><img src="img/ange1.jpg"></img></div>';
+$demon = '<div class="img"><img src="img/demon.jpg"></img></div>';
+$dragon = '<div class="img"><img src="img/dragon.jpg"></img></div>';
+$loup_garou = '<div class="img"><img src="img/loup_garou.jpg"></img></div>';
+$vampire = '<div class="img"><img src="img/vampire1.jpg"></img></div>';
+$ent = '<div class="img"><img src="img/ent.jpg"></img></div>';
+$fee = '<div class="img"><img src="img/fée.jpg"></img></div>';
+$fenrir = '<div class="img"><img src="img/fenrir.jpg"></img></div>';
+$pegase = '<div class="img"><img src="img/pegase.jpg"></img></div>';
+$valkyrie = '<div class="img"><img src="img/valkyrie.jpg"></img></div>';
+$zombi = '<div class="img"><img src="img/zombie1.jpg"></img></div>';
+$alien = '<div class="img"><img src="img/alien.jpg"></img></div>';
+
 class Card
 {
     // ATTRIBUT
@@ -15,11 +29,12 @@ class Card
     }
 
     // METHODE \\
+    // dos de la carte
     public function back()
     {
         return $this->back;
     }
-
+    // face de la carte
     public function face()
     {
         return $this->face;
@@ -29,7 +44,7 @@ class Card
     {
         return $this->back . $this->face;
     }
-
+    // retourné la carte la carte
     public function flip()
     {
         if (isset($_POST)) {
